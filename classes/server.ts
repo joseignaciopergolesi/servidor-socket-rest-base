7d44fda3339340d308e2c333a111727027d0e4d0
Jose Ignacio Pergolesi <joseignaciopergolesi@gmail.com>
2018-11-24 09:31:53 -0300

    empezando a trabajar con sockets - emito y escucho eventos para un chat

--- classes/server.ts
+++ classes/server.ts
@@ -1,22 +1,53 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
-
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
 
+    private static _intance: Server;
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server; //se encarga de los eventos de los socket
+    private httpServer: http.Server;
 
-    constructor() {
+    private constructor() {
 
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
+    }
+
+    //implemento el patron singleton para evitar tener multiples instancias de Server
+    public static get instance(){
+        return this._intance || ( this._intance = new this() );
+        //este return lo que hace es si existe una instancia la regresa y si no existe o es la primera vez que se llama a esa funcion entonces crea una nueva instancia
+    }
+
+    private escucharSockets(){
+        
+        console.log("escuchando sockets");
+
+        this.io.on('connection', cliente => {
+            console.log('Nuevo cliente conectado');
+
+            // esta pendiente de los mensajes
+            socket.mensaje( cliente, this.io);
+
+            // DESCONECTAR 
+            socket.desconectar( cliente );
+        });
+
 
     }
 
     start( callback: Function ){
-        this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
     }
 
 
---
+++ sockets/sockets.ts
@@ -0,0 +1,19 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+
+export const desconectar = ( cliente: Socket) => {
+    cliente.on( 'disconnect', () => {
+        console.log('Cliente desconectado');
+    } );
+}
+
+// Escuchar mensajes
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('mensaje', ( payload: { de: string, cuerpo: string} ) => {
+        console.log('Mensajee recibido ', payload);
+
+        // EMITO UN EVENTO A TODOS LOS USUARIOS CONECTADOS A LA APP DE SOCKETS
+        io.emit('mensaje-nuevo', payload);
+    });
+}
